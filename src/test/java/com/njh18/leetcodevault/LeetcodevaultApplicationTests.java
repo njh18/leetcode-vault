e6724f545a52c030975b4f52421a38e0893b5e55
Ng Jun Hao <ngjunhao1996@gmail.com>
2023-09-06 22:49:58 +0800

    init commit

---
+++ src/test/java/com/njh18/leetcodevault/LeetcodevaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njh18.leetcodevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodevaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
