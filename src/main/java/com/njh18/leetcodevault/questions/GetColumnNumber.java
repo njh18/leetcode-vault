10ba19abe2e78e50d0cd12e0b372f855e80ac677
Ng Jun Hao <ngjunhao1996@gmail.com>
2023-10-27 21:09:53 +0800

    Get Column Number

---
+++ src/main/java/com/njh18/leetcodevault/questions/GetColumnNumber.java
@@ -0,0 +1,28 @@
+package com.njh18.leetcodevault.questions;
+
+
+// https://workat.tech/problem-solving/practice/excel-column
+
+public class GetColumnNumber {
+
+    public static void main(String[] args) {
+        System.out.println("value is: " + getColumnNumber("Z"));
+        System.out.println("value is: " + getColumnNumber("ABCA"));
+        System.out.println("value is: " + getColumnNumber("ABC"));
+        System.out.println("value is: " + getColumnNumber("AAB"));
+    }
+
+    public static int getColumnNumber(String excelColumnNumber) {
+        int numOfAlphabets = excelColumnNumber.length();
+        int colNum = 0;
+        for (int i = 0; i < numOfAlphabets; i++) {
+            colNum += (int) Math.pow(26, i);
+        }
+        for (int i = excelColumnNumber.length() - 1; i >= 0; i--) {
+            char c = excelColumnNumber.charAt(i);
+            int diff = c - 'A';
+            colNum += Math.multiplyExact(diff, (int) Math.pow(26, excelColumnNumber.length() - 1 - i));
+        }
+        return colNum;
+    }
+}
