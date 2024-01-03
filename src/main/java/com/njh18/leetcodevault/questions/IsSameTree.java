969839aa6eabfd7f97c81cd91c2c91e72878b085
Ng Jun Hao <ngjunhao1996@gmail.com>
2024-01-03 21:02:52 +0800

    is same tree and is sub tree

---
+++ src/main/java/com/njh18/leetcodevault/questions/IsSameTree.java
@@ -0,0 +1,18 @@
+package com.njh18.leetcodevault.questions;
+
+import com.njh18.leetcodevault.model.TreeNode;
+
+public class IsSameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        // if reach here means both are not null
+        if (p == null || q == null) {
+            // here means one of it is null and that means both not the same
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
