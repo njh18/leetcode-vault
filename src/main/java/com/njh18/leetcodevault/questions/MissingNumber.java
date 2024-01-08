1a6a2fc01051949b8b3ccb219130f3b86bc4aeea
Ng Jun Hao <ngjunhao1996@gmail.com>
2024-01-08 11:59:27 +0800

    Missing Number

---
+++ src/main/java/com/njh18/leetcodevault/questions/MissingNumber.java
@@ -0,0 +1,34 @@
+package com.njh18.leetcodevault.questions;
+
+public class MissingNumber {
+
+    public static void main(String[] args) {
+        missingNumber(new int[]{9,6,4,2,3,5,7,0,1});
+        missingNumber_betterSpaceCompexity(new int[]{9,6,4,2,3,5,7,8,1});
+    }
+    public static int missingNumber(int[] nums) {
+        int[] exitingValues = new int[nums.length];
+        for (int num : nums) {
+            if (num != nums.length) {
+                exitingValues[num] = 1;
+            }
+        }
+
+        for (int i = 0; i < exitingValues.length; i++) {
+            if (exitingValues[i] == 0) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    public static int missingNumber_betterSpaceCompexity(int[] nums) {
+        int supposedSum = (nums.length + 1) * (nums.length) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum+=num;
+        }
+        return supposedSum - actualSum;
+    }
+}
