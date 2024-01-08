0b7b8a44414eadbf20e790701ae7fa60c772c8d4
Ng Jun Hao <ngjunhao1996@gmail.com>
2024-01-08 18:16:36 +0800

    Last stone weight, use priority queue

---
+++ src/main/java/com/njh18/leetcodevault/questions/LastStoneWeight.java
@@ -0,0 +1,26 @@
+package com.njh18.leetcodevault.questions;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+
+    public static void main(String[] args) {
+        System.out.println(lastStoneWeight(new int[]{2,7,4,1,8,1}));
+    }
+    public static int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int stone : stones) {
+            queue.offer(stone);
+        }
+
+        while (queue.size() > 1) {
+            int biggest = queue.poll();
+            int secondBiggest = queue.poll();
+            if (biggest != secondBiggest) {
+                queue.offer(biggest-secondBiggest);
+            }
+        }
+        return queue.isEmpty() ? 0 : queue.poll();
+    }
+}
