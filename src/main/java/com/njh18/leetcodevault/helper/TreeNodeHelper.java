918356fab1e8903ab5da3f6c73597b51bc7ec298
Ng Jun Hao <ngjunhao1996@gmail.com>
2024-02-17 18:08:41 +0800

    IsValidBST #trees #BST #recursive

--- src/main/java/com/njh18/leetcodevault/helper/TreeNodeHelper.java
+++ src/main/java/com/njh18/leetcodevault/helper/TreeNodeHelper.java
@@ -2,6 +2,9 @@ package com.njh18.leetcodevault.helper;
 
 import com.njh18.leetcodevault.model.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreeNodeHelper {
 
     public static int heightOfSubTree(TreeNode root) {
@@ -9,5 +12,19 @@ public class TreeNodeHelper {
             return 0;
         }
         return 1 + Math.max(heightOfSubTree(root.left), heightOfSubTree(root.right));
+    }
+
+    public static List<Integer> inOrderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inOrderTraversal(root, result);
+        return result;
+    }
+
+    private static void inOrderTraversal(TreeNode root, List<Integer> result) {
+        if (root != null) {
+            inOrderTraversal(root.left, result);
+            result.add(root.val);
+            inOrderTraversal(root.right, result);
+        }
     }
 }
